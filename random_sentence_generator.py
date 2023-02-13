d2545e56e1866a20f0e8fd80d59508cf74710362
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-02-13 15:41:03 +0200

    Update code
    Increased spasing for better readability

--- random_sentence_generator.py
+++ random_sentence_generator.py
@@ -8,11 +8,17 @@ def randomize_words(words):
 while True:
 
     names = input("Enter your friends' names separated by comma (eg. Jane, Eyre) then press Enter ").split(", ")
+    print()
     places = input("Enter locations separated by comma (eg. the village, space) then press Enter ").split(", ")
+    print()
     adverbs = input("Enter adverbs separated by comma (eg. calmly, rapidly) then press Enter ").split(", ")
+    print()
     verbs = input("Enter actions separated by comma (eg. cooked, sang) then press Enter ").split(", ")
+    print()
     nouns = input("Enter nouns separated by comma (eg. ice cream, guitar) then press Enter ").split(", ")
+    print()
     details = input("Enter details separated by comma (eg. at the party, on the highway) then press Enter ").split(", ")
+    print()
 
     new_game = False
 
@@ -26,8 +32,10 @@ while True:
         random_detail = randomize_words(details)
 
         print(f"{random_name} from {random_place} {random_adverb} {random_verb} {random_noun} {random_detail}")
+        print()
 
         print("Enter (Y)es to generate another random sentence, (R)estart to enter new input or (N)o to exit")
+        print()
 
         valid_input = False
 
@@ -37,9 +45,10 @@ while True:
             if user_input == "y" or user_input == "yes":
                 valid_input = True
             elif user_input == "n" or user_input == "no":
-                exit()
+                quit()
             elif user_input == "r" or user_input == "restart":
                 new_game = True
                 break
             else:
                 print("Invalid input! Please enter again.")
+                print()
